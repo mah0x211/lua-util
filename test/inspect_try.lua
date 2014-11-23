de42c07678e5783c3b2c82ec6aececbe8d3a5fca
Masatoshi Teruya <mah0x211@gmail.com>
2014-11-23 20:56:21 +0900

    add: test/inspect_try.lua has been added.

---
+++ test/inspect_try.lua
@@ -0,0 +1,47 @@
+local inspect = require('util').inspect;
+
+local fn = function() end;
+local co = coroutine.create(function() end);
+local tbl = {
+    -- boolean
+    true, false,
+    -- string
+    'hello', 'world', '',
+    -- number
+    0, 1, -1,
+    -- function
+    fn,
+    -- table
+    {},
+    -- finite
+    1/0, 
+    -- NaN
+    0/0, 
+    -- thread
+    co,
+    [co] = {
+        'test'
+    },
+    ['test'] = 'sample'
+};
+local cmp = [[
+{ 
+    [1] = true,
+    [2] = false,
+    [3] = "hello",
+    [4] = "world",
+    [5] = "",
+    [6] = 0,
+    [7] = 1,
+    [8] = -1,
+    [9] = "]] .. tostring(fn) .. [[",
+    [10] = { },
+    [11] = inf,
+    [12] = nan,
+    [13] = "]] .. tostring(co) .. [[",
+    test = "sample",
+    ["]] .. tostring(co) .. [["] = { 
+        [1] = "test"
+    }
+}]];
+ifNotEqual( inspect( tbl ), cmp );
