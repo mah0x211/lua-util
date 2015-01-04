285000cb17ff02cabfbbd0bfd873147a85dcffec
Masatoshi Teruya <mah0x211@gmail.com>
2015-01-04 22:04:23 +0900

    add: test/lastIndex_try.lua has been added.

---
+++ test/lastIndex_try.lua
@@ -0,0 +1,11 @@
+local lastIndex = require('util').table.lastIndex;
+
+ifTrue(isolate(function()
+    lastIndex();
+end));
+ifNotNil( lastIndex({ a = 1 }) );
+ifNotEqual( lastIndex({ a = 1, 2, 3 }), 2 );
+ifNotEqual( lastIndex({ a = 1, 2, 3, 'b' }), 3 );
+ifNotEqual( lastIndex({ a = 1, 2, 3, 'b', c = 4 }), 3 );
+ifNotEqual( lastIndex({ a = 1, 2, 3, 'b', c = 4, 'd' }), 4 );
+ifNotEqual( lastIndex({ a = 1, 2, 3, 'b', c = 4, 'd', 5 }), 5 );
