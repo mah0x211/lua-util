eee7bc0c3dde59a2cda70cabc6d83e4f5ad3b410
Masatoshi Teruya <mah0x211@gmail.com>
2015-01-05 13:40:15 +0900

    change: lastIndex_try.lua has been updated.

--- test/lastIndex_try.lua
+++ test/lastIndex_try.lua
@@ -9,3 +9,4 @@ ifNotEqual( lastIndex({ a = 1, 2, 3, 'b' }), 3 );
 ifNotEqual( lastIndex({ a = 1, 2, 3, 'b', c = 4 }), 3 );
 ifNotEqual( lastIndex({ a = 1, 2, 3, 'b', c = 4, 'd' }), 4 );
 ifNotEqual( lastIndex({ a = 1, 2, 3, 'b', c = 4, 'd', 5 }), 5 );
+ifNotEqual( lastIndex({ a = 1, 2, 3, 'b', c = 4, 'd', 5, [-10] = 'e', [123] = 'e' }), 123 );
